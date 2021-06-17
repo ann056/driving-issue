664711e2d71bd1b247ba2b27b3aa0f4a3c269fb4
Ann Chen <ann9975656@gmail.com>
2021-06-18 00:04:05 +0800

    Add New File

---
+++ driving.py
@@ -0,0 +1,13 @@
+driving = input('你有沒有開過車？')
+age = input('請問你的年齡？')
+age = int(age)
+if driving == '有':
+	if age >= 18:
+		print('開車小心！')
+	else:
+		print('奇怪...你怎麼會開過車？')
+if driving == '沒有':
+	if age >= 18:
+		print('你可以考駕照了啊')
+	else:
+		print('乖乖等到18歲再去考駕照吧')
